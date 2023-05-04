ecabec0f6c1788f915ee55e2cd2ea4abb79e1dfe
veronica <vmaldonado@territoriosinteligentes.net>
2023-05-04 12:34:48 -0500

    Contenido y Ejercicios de Mates Discretas y Programacion, Acerca De

--- JS/matricula.js
+++ JS/matricula.js
@@ -4,3 +4,18 @@ function redireccionarRegistro(){
 function redireccionarPaginaUsuario(){
     window.location.href = "../HTML/paginaUsuario.html"
 }
+function redireccionarContenidoMatesDiscretas(){
+    window.location.href = "../HTML/ContenidoMatesDiscretas.html"
+}
+function redireccionarContenidoProgra(){
+    window.location.href = "../HTML/contenidoProgra.html"
+}
+function redireccionarContenidoBD(){
+    window.location.href = "../HTML/ContenidoBD.html"
+}
+function redireccionarContenidoIntegral(){
+    window.location.href = "../HTML/curso_integrales_p2.html"
+}
+function redireccionarContenidoAlgebra(){
+    window.location.href = "../HTML/ContenidoAlgebra.html"
+}
